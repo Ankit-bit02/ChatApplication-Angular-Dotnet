fcb90b54b65477abb0d307a2c77478bf70817243
Ankit Karna <ankit.karna@dhunitech.io>
2025-08-21 11:01:34 +0545

    Added pwa support

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab14a3f8333a5a9eb39c14b2ca7e2a0204169f2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bf06f1d011d9001b13c87cc23b864ad63bdb2e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
